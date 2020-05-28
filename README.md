eb05d4fadc5de23af1d67a229ae328b36ecf5c03
nick <nicholas.egan@starlab.io>
2020-05-27 19:40:39 -0500

    day 1 part 1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # aoc_2019_rust
 aoc2019 in crustacean speak
+
+## Usage
+from the main repo directory
+```
+cargo run -p day1
+# ^^ to run the crate in day1
+```
---
+++ day1/src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    println!("Answer 1: {}", a::get_ans_one());
+    println!("Answer 2: {}", a::get_ans_two());
+}
+
+mod a {
+    use std::fs;
+    use std::cmp::Ordering;
+
+    pub fn get_ans_one() -> i64 { calculate_the_fuel(get_input())}
+    pub fn get_ans_two() -> i64 { calculate_the_fuel_recursive(get_input())}
+    pub fn fuel_for_mass(module_mass: i64) -> i64 { (module_mass / 3) - 2 }
+    fn get_input() -> String {
+        match fs::read_to_string("input.txt") {
+            Ok(f) => { f },
+            Err(_) => {
+                match fs::read_to_string("day1/input.txt") {
+                    Ok(fi) => { fi},
+                    Err(_) => { String::from("no") }
+                }
+            }
+
+        }
+    }
+        //fs::read_to_string("input.txt").expect("Couldn't read file")}
+
+    fn calculate_the_fuel(stringput: String) -> i64 {
+        let mut total_fuel = 0i64;
+        for line in stringput.lines() {
+            match line.parse::<i64>() {
+                Ok(i) => {total_fuel += fuel_for_mass(i);},
+                Err(_e) => {},
+            }
+        }
+        total_fuel
+    }
+    fn calculate_the_fuel_recursive(stringput: String) -> i64 {
+        let mut total_fuel = 0i64;
+        for line in stringput.lines() {
+            match line.parse::<i64>() {
+                Err(_e) => {},
+                Ok(i) => {
+                    let mut j = i;
+                    loop {
+                        if j <= 0 { break; } else {}
+
+                        total_fuel += j;
+                        j = fuel_for_mass(j);
+                    }
+                },
+            }
+        }
+        total_fuel
+    }
+
+}
